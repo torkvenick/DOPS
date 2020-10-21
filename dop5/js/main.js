da60500005d23da1b75be9af30998a7e01e5174b
torkvenick <oomsnikita@gmail.com>
2020-10-21 22:05:17 +0200

    dop5 skeleton finished

--- dop5/js/main.js
+++ dop5/js/main.js
@@ -1,20 +1,13 @@
 var mySwiper = new Swiper('.swiper-container', {
-  // Optional parameters
+ 
   loop: true,
-
-  // If we need pagination
+  effect: 'fade',
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    clickable: true,
   },
 })
